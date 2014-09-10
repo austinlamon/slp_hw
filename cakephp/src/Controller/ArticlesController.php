e02505cbac8cd7644f42bda67d434c182ac3c01e
Austin Lamon <a.lamon4@gmail.com>
2014-09-10 12:38:43 -0400

    commiting controller

--- cakephp/src/Controller/ArticlesController.php
+++ cakephp/src/Controller/ArticlesController.php
@@ -6,4 +6,12 @@ class ArticlesController extends AppController {
         $articles = $this->Articles->find('all');
         $this->set(compact('articles'));
     }
+    
+    public function view($id = null) {
+        if (!$id) {
+            throw new NotFoundException(__('Invalid article'));
+        }
+        $article = $this->Articles->get($id);
+        $this->set(compact('article'));
+    }
 }
